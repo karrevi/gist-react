f3cd928b389d904629a9d5408a937be32c5fa998
Carlos Revilla <clro26.k@gmail.com>
2020-06-07 17:41:02 +0200

    route login completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'antd/dist/antd.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Register from './containers/User/Register/Register';
+import Login from './containers/User/Login/Login.jsx';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Header />
     <Switch>
       <Route path='/register' component={Register} exact/>
+      <Route path='/login' component={Login} exact/>
     </Switch>
     </BrowserRouter>
   );
