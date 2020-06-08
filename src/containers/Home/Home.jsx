fd4aa54978e36907dd9c1793e81431ea0c24a150
Carlos Revilla <clro26.k@gmail.com>
2020-06-08 12:38:15 +0200

    call the redux state of the snnipets component and saved it in the props

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { getAllSnnipets } from '../../redux/actions/snnipets';
+import { connect } from 'react-redux';
+import './Home';
+
+const Home = (props) => {
+    useEffect(() => {
+        getAllSnnipets()
+            .catch(console.error)
+    }, [])
+    return (
+        <div className="snnipets">
+            <p>Aqui se mostrarán todos los snnipets!</p>
+        </div>
+    )
+}
+
+const mapStateToProps = ({ snnipets }) => ({ snnipets: snnipets.snnipets });
+export default connect(mapStateToProps)(Home);
