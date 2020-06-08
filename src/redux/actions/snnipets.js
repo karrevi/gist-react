3c876e1476ce0d5670f98de9801ca556b1293395
Carlos Revilla <clro26.k@gmail.com>
2020-06-08 12:34:58 +0200

    function insert snnipets

---
+++ src/redux/actions/snnipets.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import {
+    API_URL
+} from '../../api-config';
+
+
+export const getAllSnnipets = async () => {
+    const res = await axios.get(API_URL + '/snnipets')
+    store.dispatch({
+        type: 'GET_ALL_SNNIPETS',
+        payload: res.data
+    });
+    return res;
+}
+export const insert = async () => {
+    const res = await axios.post(API_URL + '/snnipets')
+    store.dispatch({
+        type: 'INSERT_SNNIPETS',
+        payload: res.data
+    });
+    return res;
+}
