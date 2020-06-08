ccd4abb34a1fb556eb3221b8de5365457f4ce71a
Carlos Revilla <clro26.k@gmail.com>
2020-06-08 20:25:26 +0200

    route modified

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Header from './components/Header/Header.jsx';
 import Register from './containers/User/Register/Register';
 import Login from './containers/User/Login/Login.jsx';
 import Home from './containers/Home/Home';
+import Profile from './containers/User/Profile/Profile';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Route path='/register' component={Register} exact/>
       <Route path='/login' component={Login} exact/>
       <Route path='/home' component={Home} exact/>
+      <Route path='/profile' component={Profile} exact />
     </Switch>
     </BrowserRouter>
   );
