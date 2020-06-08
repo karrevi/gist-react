fa1ad0fd2fc5d2ef5c5e2a55a4694257105b8d6e
Carlos Revilla <clro26.k@gmail.com>
2020-06-08 20:41:24 +0200

    modified line code

--- src/redux/actions/users.js
+++ src/redux/actions/users.js
@@ -3,6 +3,7 @@ import {
     API_URL
 } from '../../api-config';
 import store from '../store';
+// import { useHistory } from 'react-router-dom';
 
 export const register = async (user) => {
     await axios.post(API_URL + '/users/register', user);
@@ -15,14 +16,47 @@ export const login = async (credentials) => {
         payload: res.data.user
     });
 }
-export const getUserInfo = async(user) => {
+export const getUserInfo = async (user) => {
     const res = await axios.get(API_URL + '/users/info', {
-        headers:{
-                Authorization: 'Bearer  ' + localStorage.getItem('authToken')
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken')
         }
     });
     store.dispatch({
         type: 'SET_USER',
         payload: res.data
+    })
+}
+export const logout = async () => {
+    // const history = useHistory();
+    const res = await axios.get(API_URL + '/users/logout', {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken')
+        }
+    })
+    localStorage.removeItem('authToken');
+    store.dispatch({
+        type: 'LOGOUT',
+        payload: ''
+    })
+    // history.push('/')
+    return res;
+}
+export const update = async (user) => {
+    const res = await axios.post(API_URL + '/users', user, {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken')
+        }
+    });
+    store.dispatch({
+        type: 'SET_USER',
+        payload: res.data
+    });
+}
+export const uploadImage = async (user) => {
+    const res = await axios.put(API_URL + '/users', user, {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken')
+        }
     });
 }
