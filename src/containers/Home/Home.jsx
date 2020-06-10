da9d283816557cd798898639624993fa11ebe934
Carlos Revilla <clro26.k@gmail.com>
2020-06-10 17:20:18 +0200

    import moment from react-moment in component

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { getAll } from '../../redux/actions/snnipets';
 import { connect } from 'react-redux';
+import Moment from 'react-moment';
 import './Home.scss';
+import { Avatar } from 'antd';
 import { BranchesOutlined, StarFilled, CodeFilled, CommentOutlined } from '@ant-design/icons'
 
 const Home = (props) => {
@@ -22,37 +24,51 @@ const Home = (props) => {
                     <a className="select-navigation-item reponav-item" NavLink to=''><StarFilled />Starred</a>
                 </nav >
             </div>
-            <div className="gist-snnipets">
-                <div className="gits-snnipets-meta d-inline-bock width-full">
-                    <ul className="float-right h6">
-                        <li className="d-inline-block mr-3">
-                            <a className="muted-link" NavLink to=""> <CodeFilled />file</a>
-                        </li>
-                        <li className="d-inline-block mr-3">
-                            <a className="muted-link" NavLink to=""> <BranchesOutlined /> forks
-                            </a>
-                        </li>
-                        <li className="d-inline-block mr-3">
-                            <a className="muted-link" NavLink to=""> <CommentOutlined /> comentarios
-                            </a>
-                        </li>
-                        <li className="d-inline-block mr-3">
-                            <a className="muted-link" NavLink to=""> <StarFilled /> stars
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-
             {props.snnipet?.map(snnipet =>
-                <div className="snnipet" key={snnipet?._id}>
-                    <p>{snnipet.name}</p>
-                    <p>{snnipet.code_snnipets}</p>
-                    <p>{snnipet.extension}</p>
+                <div className="gist-snnipets" key={snnipet?._id}>
+                    <div className="gits-snnipets-meta d-inline-bock width-full">
+                        <ul className="float-right h6">
+                            <li className="d-inline-block mr-3">
+                                <a className="muted-link" NavLink to=""> <CodeFilled />file</a>
+                            </li>
+                            <li className="d-inline-block mr-3">
+                                <a className="muted-link" NavLink to=""> <BranchesOutlined /> forks
+                            </a>
+                            </li>
+                            <li className="d-inline-block mr-3">
+                                <a className="muted-link" NavLink to=""> <CommentOutlined /> comentarios
+                            </a>
+                            </li>
+                            <li className="d-inline-block mr-3">
+                                <a className="muted-link" NavLink to=""> <StarFilled /> stars
+                            </a>
+                            </li>
+                        </ul>
+                        <div className="float-left">
+                            <div className="d-inline-block v-align-top mt-1">
+                                <a className="d-inline-block"><Avatar src={props?.snnipet[0]?.user.image_path} />
+                                </a>
+                            </div>
+                            <div className="d-inline-block">
+                                <span>
+                                    <a data-hovercard-type="user">{props?.snnipet[0]?.user.name}</a> / <a>{snnipet.name}</a>
+                                </span>
+                                <div className="text-gray">
+                                    Creado <Moment fromNow>2020-06-19T12:59-0500</Moment>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="js-gist-file-update-container js-task-list-container file-box">
+                        <div id="file-paste" className="file">
+                            <div itempro="text" className="Box-body p-0 blod-wrapper data type-text">{snnipet.code_snnipets}
+                                <table className=""></table>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             )}
-
         </div>
     )
 }
