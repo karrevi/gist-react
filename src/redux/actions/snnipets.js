0f890b83a9ea8eae408ae3eef667253aff7ba77a
Carlos Revilla <clro26.k@gmail.com>
2020-06-10 12:53:18 +0200

    added authorization + token

--- src/redux/actions/snnipets.js
+++ src/redux/actions/snnipets.js
@@ -6,15 +6,24 @@ import store from '../store';
 
 
 export const getAll = async () => {
-    const res = await axios.get(API_URL + '/snnipets')
+    const res = await axios.get(API_URL + '/snnipets', {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken')
+        }
+    });
+    console.log(res)
     store.dispatch({
-        type: 'GET_ALL',
+        type: 'GET_ALL_SNNIPETS',
         payload: res.data
     });
     return res;
 }
 export const insert = async () => {
-    const res = await axios.post(API_URL + '/snnipets')
+    const res = await axios.post(API_URL + '/snnipets', {
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('authToken')
+        }
+    });
     store.dispatch({
         type: 'INSERT_SNNIPETS',
         payload: res.data
